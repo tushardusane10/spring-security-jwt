b1aae275cad7cc04bd90f2bccf0b4e22ab2ec288
Tushar <tushar@Tushars-MacBook-Pro.local>
2026-05-05 23:22:29 +0530

    Initial commit

---
+++ src/main/java/com/example/spring/security/jwt/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.example.spring.security.jwt.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    @PostMapping(value = "/login")
+    public ResponseEntity<String> login() {
+        return new ResponseEntity<>("Hello Worls", HttpStatus.OK);
+    }
+}
